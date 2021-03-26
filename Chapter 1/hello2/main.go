c294bd9aeb075a47dd2450d7d89c5083132ba4c1
yandongxiao <dxyan06@gmail.com>
2021-03-26 16:27:06 +0800

    update

--- Chapter 1/hello2/main.go
+++ Chapter 1/hello2/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
